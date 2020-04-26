aa9a612eef7666d0010fc7561fdbec3c12f7577b
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-04-26 15:52:05 +0900

    Submit comments & Dispaly response from server

--- myapp.rb
+++ myapp.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
+require 'json'
 require 'sinatra'
 
 get '/' do
   send_file 'index.html'
+end
+
+post '/api/comments' do
+  json = JSON.parse(request.body.read, symbolize_names: true)
+  res = {
+    comment: json[:comment],
+    created_at: Time.now.strftime('%F at %I:%M%#p')
+  }
+  res.to_json
 end
---
+++ public/index.js
@@ -0,0 +1,19 @@
+const handleSubmit = async () => {
+  const comment = document.querySelector("input#comment").value;
+  const data = {
+    comment,
+  };
+  const res = await fetch("/api/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;",
+    },
+    body: JSON.stringify(data),
+  });
+  const responseData = await res.json();
+  const responseElement = document.getElementById("api-call-result");
+  responseElement.innerText = JSON.stringify(responseData);
+
+  // Delete input value
+  document.querySelector("input#comment").value = "";
+};
