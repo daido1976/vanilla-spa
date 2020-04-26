b2961d75a7ef712ea4620c90a3d5ec66fb4552a6
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-04-26 18:43:49 +0900

    :tada: Enable to work E2E

--- myapp.rb
+++ myapp.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 require 'sinatra'
+require './pg_client'
 
 get '/' do
   send_file 'index.html'
@@ -9,11 +10,15 @@ end
 
 post '/api/posts' do
   json = JSON.parse(request.body.read, symbolize_names: true)
-  posts = [
-    {
-      comment: json[:comment],
-      created_at: Time.now.strftime('%F at %I:%M%#p')
-    }
-  ]
+  sql = <<~SQL
+    INSERT INTO posts (name, comment, created_at) VALUES ('daido', \'#{json[:comment]}\', \'#{Time.now.strftime('%F %T')}\');
+  SQL
+
+  pg = PgClient.new
+  pg.exec(sql)
+  sql = <<~SQL
+    SELECT * FROM posts;
+  SQL
+  posts = pg.exec(sql).to_a
   posts.to_json
 end
