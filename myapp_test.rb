64e872f3f1e359339b56f0627ddb5a2d5ea4f774
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-05-08 08:34:21 +0900

    Add test post api posts

--- myapp_test.rb
+++ myapp_test.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'json'
 require 'minitest/autorun'
 require 'rack/test'
 require './myapp'
@@ -15,5 +16,11 @@ class MyAppTest < Minitest::Test
     get '/'
     assert_equal 200, last_response.status
     assert_includes last_response.body, 'Hello!'
+  end
+
+  def test_post_api_posts
+    post '/api/posts', JSON.generate(comment: 'test!')
+    assert_equal 200, last_response.status
+    assert_equal 'test!', JSON.parse(last_response.body, symbolize_names: true).last[:comment]
   end
 end
