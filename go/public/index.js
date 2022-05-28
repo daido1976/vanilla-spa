f34bac34060177bec69d2eab0173ad703577dfd3
daido1976 <daido1976@gmail.com>
2022-05-28 22:17:04 +0900

    mount on load

--- go/public/index.js
+++ go/public/index.js
@@ -1,20 +1,26 @@
-document.getElementById("download").onclick = async () => {
-  const res = await fetch("/download", {
-    method: "GET",
-  });
-  const blob = await res.blob();
-  const file = window.URL.createObjectURL(blob);
-  window.location.assign(file);
-};
-
-document.getElementById("redirect").onclick = async () => {
-  await fetch("/redirect", {
-    method: "GET",
-  });
-};
-
 import { greet } from "./lib.js";
 
-document.getElementById("runjs").onclick = () => {
-  console.log(greet());
+const mount = () => {
+  document.getElementById("download").onclick = async () => {
+    const res = await fetch("/download", {
+      method: "GET",
+    });
+    const blob = await res.blob();
+    const file = window.URL.createObjectURL(blob);
+    window.location.assign(file);
+  };
+
+  document.getElementById("redirect").onclick = async () => {
+    await fetch("/redirect", {
+      method: "GET",
+    });
+  };
+
+  document.getElementById("runjs").onclick = () => {
+    console.log(greet());
+  };
 };
+
+window.addEventListener("load", () => {
+  mount();
+});
